2c141d398f19ec2d8f87f9809910910c64d3bb9f
SARO0204 <prabusaravana20@gmail.com>
2026-06-20 23:32:47 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 import mysql.connector
+import os
 
 app = Flask(__name__)
 app.secret_key = "moviebooking123"
 
 # ================= DATABASE CONNECTION =================
+
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Tamil@123",
-    database="movie_booking"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = db.cursor(buffered=True, dictionary=True)
@@ -376,4 +378,4 @@ def cancel_booking(booking_id):
     return redirect('/my_bookings')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
